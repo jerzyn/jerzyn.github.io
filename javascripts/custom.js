64def1066840b06c07b5b82ccd1af9d8dd88cbd2
Andrzej Jarzyna <andrew.jarzyna@gmail.com>
2014-10-17 01:23:12 +0200

    spaces in js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -7,35 +7,38 @@ var client = contentful.createClient({
 var create = function(entries) {
 	$.each($.grep(entries, function(_entry){ return _entry.fields.company }),
    function(key, entry) {
-     var li = $(document.createElement('li'));
-     li.append($('<h3>' + entry.fields.company + '</h3>'));
-     li.append($('<span>' + entry.fields.years + '</span>'));
-     li.append($('<h4>' + entry.fields.position + '</h4>'));
-     li.append($('<p>' + entry.fields.jobDescription + '</p>'));
-     $('.work .list').append(li);
-   });
+    var li = $(document.createElement('li'));
+    li.append($('<h3>' + entry.fields.company + '</h3>'));
+    li.append($('<span>' + entry.fields.years + '</span>'));
+    li.append($('<h4>' + entry.fields.position + '</h4>'));
+    li.append($('<p>' + entry.fields.jobDescription + '</p>'));
+    $('.work .list').append(li);
+  });
 
 	$.each($.grep(entries, function(_entry){ return _entry.fields.university }),
    function(key, entry) {
-     var li = $(document.createElement('li'));
-     li.append($('<h3>' + entry.fields.university + '</h3>'));
-     li.append($('<span>' + entry.fields.years + '</span>'));
-     li.append($('<h4>' + entry.fields.degree + '</h4>'));
-     $('.education .list').append(li);
-   });
+	   var li = $(document.createElement('li'));
+	   li.append($('<h3>' + entry.fields.university + '</h3>'));
+	   li.append($('<span>' + entry.fields.years + '</span>'));
+	   li.append($('<h4>' + entry.fields.degree + '</h4>'));
+	   $('.education .list').append(li);
+  });
+
 	$.each($.grep(entries, function(_entry){ return _entry.fields.language }),
    function(key, entry) {
      var li = $(document.createElement('li'));
      li.append($('<h3>' + entry.fields.language + '</h3>'));
      li.append($('<p>' + entry.fields.level + '</p>'));
      $('.languages .list').append(li);
-   });
+  });
+
 	$.each($.grep(entries, function(_entry){ return _entry.fields.skill }),
    function(key, entry) {
      var li = $(document.createElement('li'));
      li.append($('<h4>' + entry.fields.skill + '</h4>'));
      $('.skills .list').append(li);
-   });
+  });
+
 	$.each($.grep(entries, function(_entry){ return _entry.fields.interest }),
    function(key, entry) {
      var li = $(document.createElement('li'));
